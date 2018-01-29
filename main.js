65d4943c9adc8e3e1b347be81ba1e398e455dde1
Rafael Bortolin <rafael.bortolin@amdocs.com>
2018-01-29 09:45:22 -0200

    Formating files style.

--- main.js
+++ main.js
@@ -6,104 +6,84 @@ const electron = require('electron');
 const appConfig = require('./package');
 const iconHelper = require('./src/main/iconHelper');
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const Menu = electron.Menu;
-const MenuItem = electron.MenuItem;
-const Tray = electron.Tray;
+const {
+  app, BrowserWindow, Menu, Tray,
+} = electron;
 
 const main = {};
 let bw;
-let icon = electron.nativeImage.createFromPath(app.getAppPath() + "/src/main/resources/toilet-black.png");
+const icon = electron.nativeImage.createFromPath(`${app.getAppPath()}/src/main/resources/toilet-black.png`);
 
+function showAppClick() {
+  bw.show();
+}
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-})
-
-app.on('ready', () => {
-    app.setAppUserModelId("com.amdocs.bdc.toiletalert");
-
-    bw = new BrowserWindow({ show: false });
-    main.mainWindow = bw;
-    try {
-        bw.loadURL(`file://${app.getAppPath()}/index.html`);
-
-        bw.once('ready-to-show', (event) => {
-            setIcon(bw);
-            iconHelper.changeIcon(false);
-            event.preventDefault();
-            bw.hide();            
-            bw.tray.displayBalloon({
-                title: appConfig.appName,
-                content: "The application is running in background."
-            });
-        });
-
-        /*
-        var newMenu = new Menu();
-        newMenu.append(new MenuItem({
-            label: "Dummy1", accelerator: "Ctrl+O", click: function () {
-                console.log("Dummy1");
-                debugger;
-                editMenu.append(new MenuItem({
-                    label: "Dummy2", accelerator: "Ctrl+P", click: function () {
-                        console.log("Dummy2");
-                    }
-                }));
-            }
-        }));
-
-        var editMenu = new Menu();
-        var menubar = Menu.getApplicationMenu();
-        menubar.append(new MenuItem({ label: "NewMenu", submenu: newMenu }));
-        menubar.append(new MenuItem({ label: "Edit", submenu: editMenu }));
-        bw.setMenu(menubar);
-        */
-
-        bw.on('minimize', function (event) {
-            event.preventDefault();
-            bw.hide();
-        });
-
-    } catch (error) {
-        console.log('FATAL ERROR!')
-        console.log(error);
-    } finally {
-        bw.on('closed', () => { bw = null; });
-    }
-});
-
-function setIcon(mainWindow) {
-    mainWindow.setIcon(icon, "");
-    mainWindow.setTitle(appConfig.appName);
-    mainWindow.tray = addTray();
+function quitAppClick() {
+  app.isQuiting = true;
+  app.quit();
 }
 
 function addTray() {
-    var contextMenu = Menu.buildFromTemplate([
-        {
-            label: 'Show App', click: function () {
-                bw.show();
-            }
-        },
-        {
-            label: 'Quit', click: function () {
-                app.isQuiting = true;
-                app.quit();
-            }
-        }
-    ]);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show App', click: showAppClick,
+    },
+    {
+      label: 'Quit', click: quitAppClick,
+    },
+  ]);
 
-    main.contextMenu = contextMenu;
+  main.contextMenu = contextMenu;
 
-    var appIcon = new Tray(icon);
-    appIcon.setToolTip("No toilet being cleaned.");
-    appIcon.setTitle(appConfig.appName);
-    appIcon.setPressedImage(icon);
-    appIcon.setHighlightMode('always');
-    appIcon.setContextMenu(contextMenu);
-    return appIcon;
+  const appIcon = new Tray(icon);
+  appIcon.setToolTip('No toilet being cleaned.');
+  appIcon.setTitle(appConfig.appName);
+  appIcon.setPressedImage(icon);
+  appIcon.setHighlightMode('always');
+  appIcon.setContextMenu(contextMenu);
+  return appIcon;
 }
+
+function setIcon(mainWindow) {
+  const localMainWindow = mainWindow;
+  localMainWindow.setIcon(icon, '');
+  localMainWindow.setTitle(appConfig.appName);
+  localMainWindow.tray = addTray();
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+  app.setAppUserModelId('com.amdocs.bdc.toiletalert');
+
+  bw = new BrowserWindow({ show: false });
+  main.mainWindow = bw;
+  try {
+    bw.loadURL(`file://${app.getAppPath()}/index.html`);
+
+    bw.once('ready-to-show', (event) => {
+      setIcon(bw);
+      iconHelper.changeIcon(false);
+      event.preventDefault();
+      bw.hide();
+      bw.tray.displayBalloon({
+        title: appConfig.appName,
+        content: 'The application is running in background.',
+      });
+    });
+
+    bw.on('minimize', (event) => {
+      event.preventDefault();
+      bw.hide();
+    });
+  } catch (error) {
+    console.error('FATAL ERROR!');
+    console.log(error);
+  } finally {
+    bw.on('closed', () => { bw = null; });
+  }
+});
 
 module.exports = main;
--- src/main/scheduleChecker.js
+++ src/main/scheduleChecker.js
@@ -1,44 +1,38 @@
 const electron = require('electron');
 const getSchedule = require('./schedule');
 const iconHelper = require('./iconHelper');
-const Notification = electron.Notification;
 
+const { app } = electron;
 
-const app = electron.app;
+let isBathCleaning = false;
+function processTime() {
+  const schedules = getSchedule();
+  if (schedules === undefined) {
+    return;
+  }
+
+  let isAnyBathCleaning = false;
+
+  schedules.forEach((schedule) => {
+    if (schedule.isCleaningNow()) {
+      iconHelper.addScheduleHapening(schedule);
+      iconHelper.changeIcon(true);
+      schedule.showNotification();
+      isAnyBathCleaning = true;
+    } else {
+      schedule.hideNotification();
+      iconHelper.removeScheduleHapening(schedule);
+    }
+  });
+
+  isBathCleaning = isAnyBathCleaning;
+
+  iconHelper.changeIcon(isBathCleaning);
+}
 
 app.on('ready', () => {
+  processTime();
+  setInterval(() => {
     processTime();
-    setInterval(() => {
-        processTime();
-    }, 30000);
+  }, 30000);
 });
-
-var isBathCleaning = false;
-
-function processTime() {
-
-    let schedules = getSchedule();
-    if (schedules == undefined) {
-        return;
-    }
-
-    let isAnyBathCleaning = false;
-
-    schedules.forEach(schedule => {
-
-        if (schedule.isCleaningNow()) {
-            iconHelper.addScheduleHapening(schedule);
-            iconHelper.changeIcon(true);
-            schedule.showNotification();
-            isAnyBathCleaning = true;
-        } else {
-            schedule.hideNotification();
-            iconHelper.removeScheduleHapening(schedule);
-        }       
-
-    });
-
-    isBathCleaning = isAnyBathCleaning;
-
-    iconHelper.changeIcon(isBathCleaning);
-}
